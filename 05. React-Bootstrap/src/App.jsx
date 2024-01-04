0858683167f300e758a450d254af128bfe3c4d22
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-01-04 17:52:34 +0530

    learned about installing bootstrap and tailwind in react

---
+++ 05. React-Bootstrap/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is Heading</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 06. React-Tailwind/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-black text-white'>This is Heading</h1>
+    </>
+  )
+}
+
+export default App
