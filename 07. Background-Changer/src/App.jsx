4724c9eb7aa3ea03ca468958a1ea2ba975deaae0
darshan72 <darshankhairnar72@gmail.com>
2024-01-06 19:55:40 +0530

    completed background changer min project

--- 07. Background-Changer/src/App.jsx
+++ 07. Background-Changer/src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from 'react'
+import './App.css'
 
 function App() {
   const [color, setColor] = useState("indigo")
 
   return (
     <>
-      <h1 className="bg-green-400">This is Heading</h1>
+      <div className="box" style={{backgroundColor:color}}>
+        <div className="button-box">
+        <div className="buttons">
+          <button class="btn" onClick={() => setColor("red")} style={{backgroundColor:'red'}}>
+              Red 
+          </button>
+          <button class="btn" onClick={() => setColor("yellow")} style={{backgroundColor:'yellow'}}>
+              Yellow
+          </button>
+          <button class="btn" onClick={() => setColor("pink")} style={{backgroundColor:'pink'}}>
+              Pink
+          </button>
+          <button class="btn" onClick={() => setColor("green")} style={{backgroundColor:'green'}}>
+              Green
+          </button>
+        </div>
+        </div>
+      </div>
     </>
   )
 }
