1cc44897498153baa95752af3963716a930e3a86
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-01-22 13:13:28 +0530

    renamed folder names

--- 00. React-Core-Basics/App.js
+++ 00. React-Core-Basics/App.js
@@ -1,4 +1,16 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const h1 = React.createElement("h1",{className:"red"},"This is element created using react");
+const H1 = React.createElement("h1", { className: "red" }, "This is element created using react");
 
-root.render(h1);
+// const DivEle = (
+//     <div className="red">
+//         <p>
+//             thid is div paragraph
+//         </p>
+//     </div>
+// )
+
+console.log(React);
+console.log(ReactDOM);
+// console.log(ReactDOMServer.renderToString(H1));
+
+root.render(H1);
